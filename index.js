4add669893bf9bdde5fd48e5af9f01aae7f62156
Fausto Calviño <faustocalvino@outlook.com>
2023-12-02 19:17:20 +0100

    working on models

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-// SERVIDOR EXPRESS
-const express = require('express');
-const app = express();  
-// SERVIDOR DE SOCKET 
-const server = require('http').createServer(app);
-// CONFIGURACION DEL SOCKET SERVER
-const io = require('socket.io')(server);
+const Server = require("./models/server");
+require("dotenv").config();
 
-// DEPLOY PUBLIC DIR
+const server = new Server();
 
-app.use(express.static(__dirname+'/public'));
-
-
-io.on('connection', (socket) => { 
-    console.log(socket.id);
-   
-    socket.emit('mensaje_bienvenida', {
-        msg: `Bienvenido al chat user ${socket.id}`,
-        date: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
-    });
-
-    socket.on('mensaje_cliente',(data)=>{
-        console.log(
-            `El servidor dice ${data.nombre} : ${data.msg} a las ${data.date}`
-        )
-    })
-
-
-});
-
-
-
-
-// CORRER SERVER
-server.listen(8080,()=>console.log('app running on 8080'));
+server.execute();
